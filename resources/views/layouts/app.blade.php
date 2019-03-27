67fb92372505268f41a1f4db1e78aa3da577df71
Unknown <unknown@example.com>
2019-03-27 14:41:46 +0100

    Add hidden class

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     {{-- geocomplete --}}
-<script src="http://maps.googleapis.com/maps/api/js?key={{ env('API_GOOGLE_KEY')}}&libraries=places"></script>
+    <script src="http://maps.googleapis.com/maps/api/js?key={{ env('API_GOOGLE_KEY')}}&libraries=places"></script>
 
     {{-- <script src="jquery.geocomplete.js"></script> --}}
     <!-- Fonts -->
